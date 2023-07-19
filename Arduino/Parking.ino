0457c36c8c3ce18a012377fbdff2ef9a2e1bf37e
liamm545 <100026444+liamm545@users.noreply.github.com>
2023-07-20 04:45:15 +0900

    Update Parking.ino

--- Arduino/Parking.ino
+++ Arduino/Parking.ino
@@ -1,19 +1,18 @@
 #include <Car_Library.h>
 
-#define NUM_SENSORS 6  // Define the number of sensors
-
 int val;  // 수신될 가변 저항 값 저장할 변수
 int angle;
 int bangle;
 int speed;
 int del;
+bool parking_signal = false;
 
 // 초음파 pin
 //int trigPins[NUM_SENSORS] = {50, 48, 46, 44, 34, 32};  //정면, 우측 정면, 우측 옆면, 후면, 좌측 옆면, 좌측 정면
-//int echoPins[NUM_SENSORS] = {51, 49, 47, 45 ,35, 33};  //정면, 우측 정면, 우측 옆면, 후면, 좌측 옆면, 좌측 정면
+//int echoPins[NUM_SENSORS] = {51, 49, 47, 45 ,35, 33};  //정면, 우측 정면, 우측 옆면, 후면, 좌측 옆면, 좌측 정
 
-int trigPins = 50; //정면만
-int echoPins = 51;
+int trigPins = 46; //우측 옆면만
+int echoPins = 47;
 
 int motor_left_1 = 2;  //왼쪽 뒷바퀴
 int motor_left_2 = 3;
@@ -31,8 +30,6 @@ const char* Strings = "정면";
 
 long distances;
 
-bool a_signal_sent = false;  // Flag to track if 'a' signal has been sent
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -49,36 +46,33 @@ void setup() {
 
 }
 
+void motor_straight(speed) {
+  motor_forward(motor_right_1, motor_right_2, speed);
+  motor_backward(motor_left_1, motor_left_2, speed);
+}
 void loop() {
   val = potentiometer_Read(analogPin); 
   
   speed = 1;
+  motor_straight(speed);
   
-        distances = ultrasonic_distance(trigPins, echoPins);
-   if (distances <=90 && !a_signal_sent && distances > 10){
-    Serial.println("aaaaaaaaaaaaaaaaaaaaaaa");  // 파이썬으로 'a' 신호를 보냄
-    a_signal_sent = true;  // Update the flag
-    delay(del*1000);
-  }
-  else{
-    Serial.println("b");
-    delay(del*1000);
-  }
-
+  distances = ultrasonic_distance(trigPins, echoPins);
+  
   if (Serial.available()){
 
-//    if (distances <=900 && !a_signal_sent){
-//    Serial.write('a');  // 파이썬으로 'a' 신호를 보냄
-//    a_signal_sent = true;  // Update the flag
-//  }
+    if (distances <= "Tunning"){
+    Serial.println("P");  // 파이썬으로 주차 시작 신호인 'P' 신호를 보냄
+    motor_hold(motor_steer_1, motor_steer_2);
+    parking_signal = true;  // Update the flag
+  }
+  
     del = Serial.parseInt();
     angle = Serial.parseInt();
 
-    int angle_1 = map(angle, -16, 16, 151, 125);
+    int angle_1 = map(angle, -17, 16, 151, 120);
     
     if(speed == 0){  //멈추는 조건. 이거는 
-      motor_hold(motor_right_1, motor_right_2);
-      motor_hold(motor_left_1, motor_left_2);
+      motor_hold();
       delay(300);  //몇 초 멈춰야 하는지.
     }
 
@@ -90,8 +84,7 @@ void loop() {
     }
     
     else{   //직진 -> 평소에 쓸 친구
-          motor_forward(motor_right_1, motor_right_2, speed);
-          motor_backward(motor_left_1, motor_left_2, speed);
+       motor_straight(speed);
     }
 
     if(val <= angle_1)   //일반 주행일 때
